379ef87ad0ce50a121a24263fbfc93c25451cb3d
Admiral2000 <husniddinzoirjonov@gmail.com>
2025-09-10 13:34:54 +0500

    remove React.use(params)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,14 +2,13 @@
 
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {Badge} from "@/components/ui/badge";
+// import {Badge} from "@/components/ui/badge";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Search} from "lucide-react";
 import {useAuth} from "@/context/AuthContext";
 import React, {useEffect, useState} from "react";
 import api from "@/lib/api";
-import {id} from "postcss-selector-parser";
 import {cn} from "@/lib/utils";
 import {useRouter} from "next/navigation";
 
